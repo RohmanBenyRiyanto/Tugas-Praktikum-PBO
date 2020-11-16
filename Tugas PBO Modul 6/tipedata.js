69b04cb8f070771d21d23672ef3199a9ec03f724
Rohman <72520643+RohmanBenyRiyanto@users.noreply.github.com>
2020-11-16 20:37:09 +0700

    Add files via upload

---
+++ Tugas PBO Modul 6/tipedata.js
@@ -0,0 +1,22 @@
+//String
+var kata = 'Apel'
+var kalimat = "Inisial adalah"
+var karakter = 'A'
+
+//output
+console.log(kata + " " + kalimat + " " + karakter)
+
+//number
+var count = 25
+var cost = 1.25
+
+console.log(count + ", " + cost)
+
+//boolean
+var object = null
+console.log(object)
+
+//undefined - benar benar kosong
+var flag = undefined
+var ref
+console.log(flag)
