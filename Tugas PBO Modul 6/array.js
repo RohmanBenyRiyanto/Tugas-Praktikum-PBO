5df932ee738399311ed1d8062d5ffa2588aef7b7
Rohman <72520643+RohmanBenyRiyanto@users.noreply.github.com>
2020-11-16 20:43:18 +0700

    Add files via upload

---
+++ Tugas PBO Modul 6/array.js
@@ -0,0 +1,14 @@
+//Deklarasi 
+var arrayNew = new Array()
+
+//Input dengan push
+arrayNew.push('value2', 'value3', 10)
+
+//Deklarasi
+var array = ['value2']
+array[0] = 'Nim'
+array[1] = 19
+array[2] = 3
+
+console.log(arrayNew)
+console.log(array)
